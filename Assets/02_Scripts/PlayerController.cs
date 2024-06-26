2413e6e35ce82d8c60ab85c2924cee6cb1c49b90
Lee JaeHyun <myleje@gmail.com>
2024-06-26 17:15:08 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /*
+        외부 입력장치 값 받아오기 
+
+        1. InputManager (Legacy)
+        2. InputSystem (new)
+    */
+
+    private float v;
+    private float h;
+
     void Start()
     {
         Debug.Log("Hello World!");
