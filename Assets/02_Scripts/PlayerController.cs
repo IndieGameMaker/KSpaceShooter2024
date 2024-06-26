cd9ee6b89e8713fb509a78034907c3c52913e114
Lee JaeHyun <myleje@gmail.com>
2024-06-26 17:37:56 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -21,11 +21,8 @@ public class PlayerController : MonoBehaviour
     {
         v = Input.GetAxis("Vertical"); // -1.0f ~ 0.0f ~ +1.0f
         h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
-        // Debug.Log($"h= {h} / v= {v} ");
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Jump !!!");
-        }
+        transform.position += new Vector3(0, 0, 0.01f);
+        // transform.position = transform.position + new Vector3(0, 0, 0.01f);
     }
 }
