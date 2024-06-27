98e9f93118c69555071a765bf457f604320cfb3f
Lee JaeHyun <myleje@gmail.com>
2024-06-27 15:42:21 +0900

    .

--- Assets/02_Scripts/WeaponController.cs
+++ Assets/02_Scripts/WeaponController.cs
@@ -5,13 +5,12 @@ public class WeaponController : MonoBehaviour
     [SerializeField] private Transform firePos;  // 총알이 생성될 위치정보
     [SerializeField] private GameObject bulletPrefab;  // 총알 프리팹
 
-    void Start()
-    {
-
-    }
-
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 총알을 동적으로 생성
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }
     }
 }
