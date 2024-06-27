9f243cef58d4b0508570da987ba854f3f9dc8e57
Lee JaeHyun <myleje@gmail.com>
2024-06-27 16:00:50 +0900

    .

---
+++ Assets/02_Scripts/RemoveBullet.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision coll)
+    {
+        Debug.Log(coll.gameObject.name);
+    }
+}
+
+/*
+    Collider 컴포넌트의 IsTrigger 언체크
+
+    OnCollisionEnter  => 1
+    OnCollisionStay   => n
+    OnCollisionExit   => 1
+
+    Collider 컴포넌트의 IsTrigger 체크
+
+    OnTriggerEnter
+    OnTriggerStay
+    OnTriggerExit
+
+    # 충돌콜백함수가 호출되는 필수조건
+
+     1. 양쪽 객체에 Collider 컴포넌트를 보유
+     2. 이동하는 객체에는 반드시 Rigidbody 컴포넌트가 있어야 함
+
+*/
