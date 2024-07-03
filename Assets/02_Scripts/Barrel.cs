729339f20b480c02dad74a25d0edaab3a0534cff
Lee JaeHyun <myleje@gmail.com>
2024-07-03 14:20:44 +0900

    .

--- Assets/02_Scripts/Barrel.cs
+++ Assets/02_Scripts/Barrel.cs
@@ -18,7 +18,12 @@ public class Barrel : MonoBehaviour
 
     private void ExpBarrel()
     {
+        // Random Impact Position 
+        Vector3 impactPoint = UnityEngine.Random.insideUnitSphere * 1.5f;
+
         var rb = this.gameObject.AddComponent<Rigidbody>();
-        rb.AddExplosionForce(1500.0f, transform.position, 5.0f, 1800.0f);
+        rb.AddExplosionForce(1500.0f, transform.position + impactPoint, 5.0f, 1800.0f);
+
+        Destroy(this.gameObject, 2.5f);
     }
 }
