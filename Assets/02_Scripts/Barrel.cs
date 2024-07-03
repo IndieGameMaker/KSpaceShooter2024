570cbcea8b8cb0b84239aaca5ccdbfbc93fee6a5
Lee JaeHyun <myleje@gmail.com>
2024-07-03 14:52:11 +0900

    .

--- Assets/02_Scripts/Barrel.cs
+++ Assets/02_Scripts/Barrel.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 public class Barrel : MonoBehaviour
 {
     [SerializeField] private GameObject expEffect;
+    [SerializeField] private Texture[] textures;
     private int hitCount;
+    private new MeshRenderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponentInChildren<MeshRenderer>();
+
+        int idx = UnityEngine.Random.Range(0, textures.Length); // 0, 1, 2
+        renderer.material.mainTexture = textures[idx];
+    }
 
     void OnCollisionEnter(Collision coll)
     {
@@ -26,6 +36,7 @@ public class Barrel : MonoBehaviour
         rb.AddExplosionForce(1500.0f, transform.position + impactPoint, 5.0f, 1800.0f);
 
         Destroy(this.gameObject, 2.5f);
-        Instantiate(expEffect, transform.position, Quaternion.identity);
+        var obj = Instantiate(expEffect, transform.position, Quaternion.identity);
+        Destroy(obj, 5.0f);
     }
 }
