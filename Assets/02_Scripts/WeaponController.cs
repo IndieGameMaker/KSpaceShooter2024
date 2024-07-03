ffe3b6c964619b42ffec889edbdef87f9e787d27
Lee JaeHyun <myleje@gmail.com>
2024-07-03 10:25:17 +0900

    .

--- Assets/02_Scripts/WeaponController.cs
+++ Assets/02_Scripts/WeaponController.cs
@@ -4,6 +4,7 @@ public class WeaponController : MonoBehaviour
 {
     [SerializeField] private Transform firePos;  // 총알이 생성될 위치정보
     [SerializeField] private GameObject bulletPrefab;  // 총알 프리팹
+    [SerializeField] private AudioClip fireSfx;  // 총소리 오디오 클립
 
     void Update()
     {
