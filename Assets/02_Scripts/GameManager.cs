7cab279dd63364dbc47a71a9f0c6f05e92c1bc97
Lee JaeHyun <myleje@gmail.com>
2024-07-10 11:52:39 +0900

    .

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -1,16 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private List<Transform> points = new List<Transform>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GameObject.Find("SpawnPointGroup")?.GetComponentsInChildren<Transform>(points);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
