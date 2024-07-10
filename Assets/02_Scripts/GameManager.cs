f84e746826907db29b3a3a818ac44a3ad33bf491
Lee JaeHyun <myleje@gmail.com>
2024-07-10 13:42:00 +0900

    .

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +7,25 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject monsterPrefab;
     [SerializeField] private List<Transform> points = new List<Transform>();
 
+    public bool isGameOver = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         GameObject.Find("SpawnPointGroup")?.GetComponentsInChildren<Transform>(points);
-
-        InvokeRepeating(nameof(CreateMonsters), 2.0f, 3.0f);
+        StartCoroutine(CreateMonsters());
+        //InvokeRepeating(nameof(CreateMonsters), 2.0f, 3.0f);
     }
 
-    void CreateMonsters()
+    IEnumerator CreateMonsters()
     {
-        int idx = UnityEngine.Random.Range(1, points.Count);
-        Instantiate(monsterPrefab, points[idx].position, Quaternion.identity);
+        yield return new WaitForSeconds(2.0f);
+
+        while (!isGameOver)
+        {
+            int idx = UnityEngine.Random.Range(1, points.Count);
+            Instantiate(monsterPrefab, points[idx].position, Quaternion.identity);
+            yield return new WaitForSeconds(3.0f);
+        }
     }
 }
