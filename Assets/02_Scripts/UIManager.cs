29704b66a06c103dae168d6d71f0c0555b40523e
Lee JaeHyun <myleje@gmail.com>
2024-07-10 16:24:02 +0900

    .

--- Assets/02_Scripts/UIManager.cs
+++ Assets/02_Scripts/UIManager.cs
@@ -1,8 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public Button startButton;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(() => StartGame());
+
+        // startButton.onClick.AddListener(() =>
+        // {
+        //     StartGame();
+        // });
+
+        // startButton.onClick.AddListener(delegate ()
+        // {
+        //     StartGame();
+        //     Debug.Log("Start Button 클릭됨");
+        // });
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Logic");
